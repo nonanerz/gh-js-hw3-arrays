7b42eb6a9daa010f248da92bf0078e5dbf663131
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-21 18:49:58 +0300

    probably done with map

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var arr = [1,2,2,2,1,4];
 
-Array.prototype.myForEach = function myForEach(callback) {
+// Array.prototype.myForEach = function myForEach(callback) {
+//     for (var i = 0; i <= this.length - 1; i++) {
+//         callback(this[i]);
+//     }
+// }
+//
+// arr.myForEach(function (item) {
+//     console.log('result- ' + item)
+// })
+
+Array.prototype.myMap = function myMap(callback) {
     for (var i = 0; i <= this.length - 1; i++) {
-        callback(this[i]);
+        this[i] = callback(this[i]);
     }
+
+    return this;
 }
 
-arr.myForEach(function (item) {
-    console.log('result- ' + item)
+arr.myMap(function (item) {
+    console.log(100 + item)
 })
-
