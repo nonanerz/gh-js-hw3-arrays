fc550923f8772aba56e0814f8e6b401b5c43530f
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-21 21:10:39 +0300

    start working on sort

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-var arr = [4,1,2,3];
+// var arr = [4,1,2,3];
+//
+// Array.prototype.myForEach = function myForEach(callback) {
+//     for (var i = 0; i <= this.length - 1; i++) {
+//         callback(this[i]);
+//     }
+// }
+//
+// arr.myForEach(function (item) {
+//     console.log('result- ' + item)
+// })
+//
+// Array.prototype.myMap = function myMap(callback) {
+//     for (var i = 0; i <= this.length - 1; i++) {
+//         this[i] = callback(this[i]);
+//     }
+//
+//     return this;
+// }
+//
+// arr.myMap(function (item) {
+//     return 100 + item
+// })
+//
+// console.log(arr)
 
-Array.prototype.myForEach = function myForEach(callback) {
-    for (var i = 0; i <= this.length - 1; i++) {
-        callback(this[i]);
+
+var arr1 = [4,5,2,3];
+
+Array.prototype.mySort = function mySort(callback) {
+    for (var i = 0; i <= this.length - 2; i++) {
+        var curr = this[i]
+            next = this[i + 1]
     }
 }
 
-arr.myForEach(function (item) {
-    console.log('result- ' + item)
-})
-
-Array.prototype.myMap = function myMap(callback) {
-    for (var i = 0; i <= this.length - 1; i++) {
-        this[i] = callback(this[i]);
-    }
-
-    return this;
-}
-
-arr.myMap(function (item) {
-    console.log(100 + item)
-})
+arr1.mySort()
 
 
-Array.prototype.mySort = function mySort() {
-    //have no idea
-}
-
-arr.mySort()
-
-console.log(arr)
+console.log(arr1)
 
