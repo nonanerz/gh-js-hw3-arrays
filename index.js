f5778e896b462daa83d9231a00d5fbc4360e121d
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-21 18:47:37 +0300

    done with foreach

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 var arr = [1,2,2,2,1,4];
 
-function myForEach(callback) {
-    for (var i = 0; i <= array.length - 1; i++) {
-        callback(array[i]);
+Array.prototype.myForEach = function myForEach(callback) {
+    for (var i = 0; i <= this.length - 1; i++) {
+        callback(this[i]);
     }
 }
 
-myForEach(arr, function (item) {
+arr.myForEach(function (item) {
     console.log('result- ' + item)
-});
-
-function myMap(array, callback) {
-    for (var i = 0; i <= array.length - 1; i++) {
-        array[i] = (callback(this[i]))
-    }
-}
-
-myMap(arr, function (item) {
-    return 10 + item
 })
-
-console.log(arr);
 
