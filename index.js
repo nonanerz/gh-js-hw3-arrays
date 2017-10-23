379ea71dbb76347f68fedd26e4b8cd9e8682a28b
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-23 15:01:00 +0300

    add callback

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-var arr = [4,1,2,3];
-
-Array.prototype.myForEach = function myForEach(callback) {
-    for (var i = 0; i <= this.length - 1; i++) {
-        callback(this[i], i, this);
-    }
-}
-
-arr.myForEach(function (item) {
-    console.log('result- ' + item)
-})
-
-Array.prototype.myMap = function myMap(callback) {
-    for (var i = 0; i <= this.length - 1; i++) {
-        this[i] = callback(this[i], i, this);
-    }
-
-    return this;
-}
-
-arr.myMap(function (item) {
-    return 100 + item
-})
-
-console.log(arr)
+// var arr = [4,1,2,3];
+//
+// Array.prototype.myForEach = function myForEach(callback) {
+//     for (var i = 0; i <= this.length - 1; i++) {
+//         callback(this[i], i, this);
+//     }
+// }
+//
+// arr.myForEach(function (item) {
+//     console.log('result- ' + item)
+// })
+//
+// Array.prototype.myMap = function myMap(callback) {
+//     for (var i = 0; i <= this.length - 1; i++) {
+//         this[i] = callback(this[i], i, this);
+//     }
+//
+//     return this;
+// }
+//
+// arr.myMap(function (item) {
+//     return 100 + item
+// })
+//
+// console.log(arr)
 
 
 var arr1 = [7,4,7,5,9,8,2,1,9,3,6];
 
-function mySort(array) {
-    for (var i = 0; i <= array.length - 2; i++) {
-        var curr = array[i]
-        var next = array[i + 1]
+Array.prototype.mySort = function mySort(callback) {
+    for (var i = 0; i <= this.length - 2; i++) {
+
+        var curr = this[i]
+        var next = this[i + 1]
 
         if (curr > next) {
-            array[i] = next
-            array[i + 1] = curr
-            mySort(array)
+            if (callback) {
+                var result = callback(curr, next)
+
+                if (result > 0) {
+                    this[i] = next
+                    this[i + 1] = curr
+                    this.mySort(callback)
+                } else if (result < 0) {
+                    this[i] = curr
+                    this[i + 1] = next
+                    this.mySort(callback)
+                }
+
+            } else {
+                this[i] = next
+                this[i + 1] = curr
+                this.mySort()
+
+            }
         }
     }
-    return array
+    return this
 }
 
-console.log(mySort(arr1))
+
+
+console.log(arr1.mySort(function(a, b){return a - b}))
+console.log(arr1.mySort())
 
 
 
